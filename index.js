113dee69623dfd0749c25365e8e40192bc6bb0c6
Xcilik <149920327+Xcilik@users.noreply.github.com>
2025-07-12 12:24:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,34 +5,28 @@ const path = require('path');
 const axios = require('axios');
 const chalk = require('chalk');
 const express = require('express');
-const readline = require('readline');
 const { createServer } = require('http');
 const { Boom } = require('@hapi/boom');
 const NodeCache = require('node-cache');
-const { toBuffer, toDataURL } = require('qrcode');
-const { exec, spawn, execSync } = require('child_process');
-const { parsePhoneNumber } = require('awesome-phonenumber');
-const { default: WAConnection, useMultiFileAuthState, Browsers, DisconnectReason, makeInMemoryStore, makeCacheableSignalKeyStore, fetchLatestBaileysVersion, proto, getAggregateVotesInPollMessage } = require('baileys');
-
-const pairingCode = process.argv.includes('--qr') ? false : process.argv.includes('--pairing-code') || global.pairing_code;
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
-const question = (text) => new Promise((resolve) => rl.question(text, resolve))
-let app = express();
-let server = createServer(app);
-let PORT = process.env.PORT || process.env.SERVER_PORT || 3000;
-let pairingStarted = false;
-
-global.fetchApi = async (path = '/', query = {}, options) => {
-	const urlnya = (options?.name || options ? ((options?.name || options) in global.APIs ? global.APIs[(options?.name || options)] : (options?.name || options)) : global.APIs['hitori'] ? global.APIs['hitori'] : (options?.name || options)) + path + (query ? '?' + decodeURIComponent(new URLSearchParams(Object.entries({ ...query }))) : '')
-	const { data } = await axios.get(urlnya, { ...((options?.name || options) ? {} : { headers: { 'accept': 'application/json', 'x-api-key': global.APIKeys[global.APIs['hitori']]}})})
-	return data
-}
+const { toBuffer } = require('qrcode');
+const { exec } = require('child_process');
+const { default: WAConnection, useMultiFileAuthState, Browsers, DisconnectReason, makeInMemoryStore, makeCacheableSignalKeyStore, fetchLatestBaileysVersion } = require('baileys');
 
 const DataBase = require('./src/database');
 const packageInfo = require('./package.json');
 const database = new DataBase(global.tempatDB);
 const msgRetryCounterCache = new NodeCache();
 const groupCache = new NodeCache({ stdTTL: 5 * 60, useClones: false });
+
+let app = express();
+let server = createServer(app);
+let PORT = process.env.PORT || process.env.SERVER_PORT || 3000;
+
+global.fetchApi = async (path = '/', query = {}, options) => {
+	const urlnya = (options?.name || options ? ((options?.name || options) in global.APIs ? global.APIs[(options?.name || options)] : (options?.name || options)) : global.APIs['hitori'] ? global.APIs['hitori'] : (options?.name || options)) + path + (query ? '?' + decodeURIComponent(new URLSearchParams(Object.entries({ ...query }))) : '')
+	const { data } = await axios.get(urlnya, { ...((options?.name || options) ? {} : { headers: { 'accept': 'application/json', 'x-api-key': global.APIKeys[global.APIs['hitori']]}})})
+	return data
+}
 
 app.get('/', (req, res) => {
 	if (process.send) {
@@ -58,18 +52,12 @@ server.listen(PORT, () => {
 const { GroupCacheUpdate, GroupParticipantsUpdate, MessagesUpsert, Solving } = require('./src/message');
 const { isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, sleep } = require('./lib/function');
 
-/*
-	* Create By Farid
-	* Follow https://github.com/fariddev
-	* Whatsapp : https://whatsapp.com/channel/0029VaWOkNm7DAWtkvkJBK43
-*/
-
 async function startFaridBot() {
 	const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
 	const { state, saveCreds } = await useMultiFileAuthState('fariddev');
 	const { version, isLatest } = await fetchLatestBaileysVersion();
 	const level = pino({ level: 'silent' });
-	
+
 	try {
 		const loadData = await database.read()
 		if (loadData && Object.keys(loadData).length === 0) {
@@ -88,7 +76,6 @@ async function startFaridBot() {
 		} else {
 			global.db = loadData
 		}
-		
 		setInterval(async () => {
 			if (global.db) await database.write(global.db)
 		}, 30 * 1000)
@@ -96,7 +83,7 @@ async function startFaridBot() {
 		console.log(e)
 		process.exit(1)
 	}
-	
+
 	const getMessage = async (key) => {
 		if (store) {
 			const msg = await store.loadMessage(key.remoteJid, key.id);
@@ -106,7 +93,7 @@ async function startFaridBot() {
 			conversation: 'Halo Saya Ti Assistant'
 		}
 	}
-	
+
 	const farid = WAConnection({
 		logger: level,
 		getMessage,
@@ -115,11 +102,9 @@ async function startFaridBot() {
 		msgRetryCounterCache,
 		retryRequestDelayMs: 10,
 		connectTimeoutMs: 60000,
-		printQRInTerminal: !pairingCode,
-		defaultQueryTimeoutMs: undefined,
+		printQRInTerminal: true,
 		browser: Browsers.ubuntu('Chrome'),
 		generateHighQualityLinkPreview: true,
-		//waWebSocketUrl: 'wss://web.whatsapp.com/ws',
 		cachedGroupMetadata: async (jid) => groupCache.get(jid),
 		transactionOpts: {
 			maxCommitRetries: 10,
@@ -134,137 +119,86 @@ async function startFaridBot() {
 			keys: makeCacheableSignalKeyStore(state.keys, level),
 		},
 	})
-	
+
 	store.bind(farid.ev)
-	
 	await Solving(farid, store)
-	
 	farid.ev.on('creds.update', saveCreds)
-	
+
 	farid.ev.on('connection.update', async (update) => {
-		const { qr, connection, lastDisconnect, isNewLogin, receivedPendingNotifications } = update
-		if ((connection == 'connecting' || !!qr) && pairingCode && !farid.authState.creds.registered && !pairingStarted) {
-			pairingStarted = true;
-			let phoneNumber;
-			async function getPhoneNumber() {
-				phoneNumber = global.number_bot ? global.number_bot : await question('Please type your WhatsApp number : ');
-				phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
-				
-				if (!parsePhoneNumber(phoneNumber).valid && phoneNumber.length < 6) {
-					console.log(chalk.bgBlack(chalk.redBright('Start with your Country WhatsApp code') + chalk.whiteBright(',') + chalk.greenBright(' Example : 62xxx')));
-					await getPhoneNumber()
-				}
-			}
-			
-			setTimeout(async () => {
-				await getPhoneNumber()
-				await exec('rm -rf ./fariddev/*')
-				console.log('Requesting Pairing Code...')
-				await new Promise(resolve => setTimeout(resolve, 5000));
-				let code = await farid.requestPairingCode(phoneNumber);
-				console.log(`Your Pairing Code : ${code}`);
-			}, 3000)
-		}
-		if (connection == 'close') {
-			const reason = new Boom(lastDisconnect?.error)?.output.statusCode
-			if (reason === DisconnectReason.connectionLost) {
-				console.log('Connection to Server Lost, Attempting to Reconnect...');
-				startFaridBot()
-			} else if (reason === DisconnectReason.connectionClosed) {
-				console.log('Connection closed, Attempting to Reconnect...');
-				startFaridBot()
-			} else if (reason === DisconnectReason.restartRequired) {
-				console.log('Restart Required...');
-				startFaridBot()
-			} else if (reason === DisconnectReason.timedOut) {
-				console.log('Connection Timed Out, Attempting to Reconnect...');
-				startFaridBot()
-			} else if (reason === DisconnectReason.badSession) {
-				console.log('Delete Session and Scan again...');
-				startFaridBot()
-			} else if (reason === DisconnectReason.connectionReplaced) {
-				console.log('Close current Session first...');
-			} else if (reason === DisconnectReason.loggedOut) {
-				console.log('Scan again and Run...');
-				exec('rm -rf ./fariddev/*')
-				process.exit(1)
-			} else if (reason === DisconnectReason.forbidden) {
-				console.log('Connection Failure, Scan again and Run...');
-				exec('rm -rf ./fariddev/*')
-				process.exit(1)
-			} else if (reason === DisconnectReason.multideviceMismatch) {
-				console.log('Scan again...');
-				exec('rm -rf ./fariddev/*')
-				process.exit(0)
-			} else {
-				farid.end(`Unknown DisconnectReason : ${reason}|${connection}`)
-			}
-		}
-		if (connection == 'open') {
-			console.log('Connected to : ' + JSON.stringify(farid.user, null, 2));
-			let botNumber = await farid.decodeJid(farid.user.id);
-			if (global.db?.set[botNumber] && !global.db?.set[botNumber]?.join) {
-				if (my.ch.length > 0 && my.ch.includes('@newsletter')) {
-					if (my.ch) await farid.newsletterMsg(my.ch, { type: 'follow' }).catch(e => {})
-					db.set[botNumber].join = true
-				}
-			}
-		}
+		const { qr, connection, lastDisconnect, isNewLogin, receivedPendingNotifications } = update;
+
 		if (qr) {
 			app.use('/qr', async (req, res) => {
-				res.setHeader('content-type', 'image/png')
-				res.end(await toBuffer(qr))
+				res.setHeader('content-type', 'image/png');
+				res.end(await toBuffer(qr));
 			});
 		}
-		if (isNewLogin) console.log(chalk.green('New device login detected...'))
-		if (receivedPendingNotifications == 'true') {
-			console.log('Please wait About 1 Minute...')
-			farid.ev.flush()
+
+		if (connection === 'close') {
+			const reason = new Boom(lastDisconnect?.error)?.output.statusCode
+			if (reason === DisconnectReason.connectionLost || reason === DisconnectReason.connectionClosed || reason === DisconnectReason.timedOut || reason === DisconnectReason.restartRequired || reason === DisconnectReason.badSession) {
+				console.log('Reconnecting...');
+				startFaridBot()
+			} else if ([DisconnectReason.loggedOut, DisconnectReason.forbidden, DisconnectReason.multideviceMismatch].includes(reason)) {
+				console.log('Session ended. Please scan QR again.');
+				exec('rm -rf ./fariddev/*')
+				process.exit(1)
+			} else {
+				farid.end(`Unknown DisconnectReason: ${reason} | ${connection}`);
+			}
+		}
+
+		if (connection === 'open') {
+			console.log('Connected to : ' + JSON.stringify(farid.user, null, 2));
+		}
+
+		if (isNewLogin) console.log(chalk.green('New device login detected...'));
+		if (receivedPendingNotifications === 'true') {
+			console.log('Please wait About 1 Minute...');
+			farid.ev.flush();
 		}
 	});
-	
+
 	farid.ev.on('contacts.update', (update) => {
 		for (let contact of update) {
 			let id = farid.decodeJid(contact.id)
 			if (store && store.contacts) store.contacts[id] = { id, name: contact.notify }
 		}
 	});
-	
+
 	farid.ev.on('call', async (call) => {
 		let botNumber = await farid.decodeJid(farid.user.id);
 		if (global.db?.set[botNumber]?.anticall) {
 			for (let id of call) {
 				if (id.status === 'offer') {
-					let msg = await farid.sendMessage(id.from, { text: `Saat Ini, Kami Tidak Dapat Menerima Panggilan ${id.isVideo ? 'Video' : 'Suara'}.\nJika @${id.from.split('@')[0]} Memerlukan Bantuan, Silakan Hubungi Owner :)`, mentions: [id.from]});
-					await farid.sendContact(id.from, global.owner, msg);
-					await farid.rejectCall(id.id, id.from)
+					let msg = await farid.sendMessage(id.from, { text: `Maaf, saya tidak menerima panggilan.` });
+					await farid.rejectCall(id.id, id.from);
 				}
 			}
 		}
 	});
-	
+
 	farid.ev.on('messages.upsert', async (message) => {
 		await MessagesUpsert(farid, message, store, groupCache);
 	});
-	
+
 	farid.ev.on('groups.update', async (update) => {
 		await GroupCacheUpdate(farid, update, store, groupCache);
 	});
-	
+
 	farid.ev.on('group-participants.update', async (update) => {
 		await GroupParticipantsUpdate(farid, update, store, groupCache);
 	});
-	
+
 	setInterval(async () => {
-		await farid.sendPresenceUpdate('available', farid.decodeJid(farid.user.id)).catch(e => {})
+		await farid.sendPresenceUpdate('available', farid.decodeJid(farid.user.id)).catch(() => {})
 	}, 10 * 60 * 1000);
 
-	return farid
+	return farid;
 }
 
 startFaridBot()
 
-// Process Exit
 process.on('exit', async () => {
 	if (global.db) await database.write(global.db)
 	console.log('Cleaning up... Closing server.');
